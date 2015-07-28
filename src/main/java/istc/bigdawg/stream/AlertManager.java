e7c667b6b870a17af835f39887adb55a2d017c10
Aaron Elmore <aelmore@csail.mit.edu>
2015-07-28 10:04:09 -0400

    WIP on hooks for SStore alert

--- src/main/java/istc/bigdawg/stream/AlertManager.java
+++ src/main/java/istc/bigdawg/stream/AlertManager.java
@@ -1,18 +1,66 @@
 package istc.bigdawg.stream;
 
+import istc.bigdawg.AuthorizationRequest;
+import istc.bigdawg.exceptions.AlertException;
+import istc.bigdawg.properties.BigDawgConfigProperties;
 import istc.bigdawg.stream.StreamDAO.DBAlert;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.StringRequestEntity;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class AlertManager {
 
 
-	public static void RegisterDBEvent(DBAlert alert){
-		System.out.println("TODO : " + alert.stroredProc + " :: " + alert.receiveURL );
-		//TODO
+	public static void RegisterDBEvent(DBAlert alert) throws AlertException{
+		ObjectMapper mapper = new ObjectMapper();
+		try{
+			RegisterStreamRequest sStoreRequest = new RegisterStreamRequest();
+			sStoreRequest.setAuthorization(new AuthorizationRequest());
+			sStoreRequest.setNotifyURL(alert.receiveURL);
+			sStoreRequest.setOneTime(alert.oneTime);
+			sStoreRequest.setQuery(alert.stroredProc);
+			String reqString = mapper.writeValueAsString(sStoreRequest);
+			System.out.println("REQ: "+ reqString);
+			PostMethod post = new PostMethod(BigDawgConfigProperties.INSTANCE.getsStoreURL());
+			System.out.println("Post to" + post.toString());
+			StringRequestEntity requestEntity = new StringRequestEntity(
+					reqString, "application/json", "UTF-8");
+			post.setRequestEntity(requestEntity);
+			HttpClient client = new HttpClient();
+			int returnCode = client.executeMethod(post);
+			System.out.println("RESP : " + returnCode);
+			if (returnCode != 200){
+				throw new AlertException();
+			}
+		} catch (Exception ex){
+			ex.printStackTrace();
+			throw new AlertException(ex);
+		}
+		
 	}
 	public static void PushEvents(List<String> urls){
-		System.out.println("TODO");
-		//TODO
+
+		HttpClient client = new HttpClient();
+		for (String u: urls){
+			GetMethod get = new GetMethod(u);
+			try {
+				System.out.println("Calling : "+u);
+				int returnCode = client.executeMethod(get);
+				System.out.println(returnCode);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 }
