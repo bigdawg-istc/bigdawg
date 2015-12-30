49df09a06b7b1b4696315df3f4ac49ec270fe04e
Adam Dziedzic <adam.dziedzi@gmail.com>
2015-12-30 13:28:05 +0100

    changed catalog database name from bigdawag_catalog to bigdawg

--- src/main/resources/bigdawg_catalog_data.sql
+++ src/main/resources/bigdawg_catalog_data.sql
@@ -3,6 +3,6 @@ insert into catalog.engines values (1,'postgres1','localhost',5431,'engine for b
 insert into catalog.engines values (2,'postgres2','localhost',5430,'main engine for mimic2_copy data');
 
 -- add catalog.databases inside the database instances
-insert into catalog.databases values(1,1,'bigdawg_catalog','postgres','test');
+insert into catalog.databases values(1,1,'bigdawg','postgres','test');
 insert into catalog.databases values(2,1,'mimic2','pguser','test');
 insert into catalog.databases values(3,2,'mimic2_copy','pguser','test');
