5fc77ee0181ebba1faa563b1e91dc13ac2fbe4af
Adam Dziedzic <adam.dziedzi@gmail.com>
2016-02-18 23:22:44 -0600

    close the resources (e.g. connection,statement,result set) in the tests for migration from PostgreSQL to SciDB

--- src/test/java/istc/bigdawg/migration/FromPostgresToSciDBTest.java
+++ src/test/java/istc/bigdawg/migration/FromPostgresToSciDBTest.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -16,6 +17,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.postgresql.copy.CopyManager;
 import org.postgresql.core.BaseConnection;
+import org.scidb.jdbc.IResultSetWrapper;
 
 import istc.bigdawg.postgresql.PostgreSQLConnectionInfo;
 import istc.bigdawg.postgresql.PostgreSQLConnectionInfoTest;
@@ -33,8 +35,8 @@ public class FromPostgresToSciDBTest {
 	private FromPostgresToSciDB migrator = new FromPostgresToSciDB();
 	private PostgreSQLConnectionInfo conFrom = new PostgreSQLConnectionInfoTest();
 	private String fromTable = "region_test_from_13241";
-	private SciDBConnectionInfo conTo = new SciDBConnectionInfo("localhost",
-			"1239", "scidb", "mypassw", "/opt/scidb/14.12/bin/");
+	private SciDBConnectionInfo conTo = new SciDBConnectionInfo("localhost", "1239", "scidb", "mypassw",
+			"/opt/scidb/14.12/bin/");
 	private String toArray = "region";
 	private long numberOfRowsPostgres = 0;
 
@@ -47,8 +49,7 @@ public class FromPostgresToSciDBTest {
 		Connection con = PostgreSQLHandler.getConnection(conFrom);
 		con.setAutoCommit(false);
 		CopyManager cpTo = new CopyManager((BaseConnection) con);
-		InputStream input = FromPostgresToSciDBTest.class.getClassLoader()
-				.getResourceAsStream("region.csv");
+		InputStream input = FromPostgresToSciDBTest.class.getClassLoader().getResourceAsStream("region.csv");
 		// FileInputStream input = new FileInputStream(new
 		// File("./region.csv"));
 		// CHECK IF THE INPUT STREAM CONTAINS THE REQUIRED DATA
@@ -57,29 +58,41 @@ public class FromPostgresToSciDBTest {
 		// input.read(buffer, 0, size);
 		// String in = new String(buffer, StandardCharsets.UTF_8);
 		// System.out.println(in);
-		numberOfRowsPostgres = cpTo.copyIn(
-				"Copy " + fromTable
-						+ " from STDIN with (format csv, delimiter '|')",
-				input);
+		numberOfRowsPostgres = cpTo.copyIn("Copy " + fromTable + " from STDIN with (format csv, delimiter '|')", input);
 		con.commit();
 		con.close();
 		assertEquals(5, numberOfRowsPostgres);
 	}
 
 	@Test
-	public void testFromPostgresToSciDBcsvSingleThreadMigration()
-			throws SQLException {
+	public void testFromPostgresToSciDBcsvSingleThreadMigration() throws SQLException {
 		migrator.migrateSingleThreadCSV(conFrom, fromTable, conTo, toArray);
 		Connection con = SciDBHandler.getConnection(conTo);
 		Statement query = con.createStatement();
-		ResultSet resultSet = query
-				.executeQuery("select * from " + toArray);
+		ResultSet resultSet = query.executeQuery("select * from " + toArray);
+		ResultSetMetaData meta = resultSet.getMetaData();
+
+		System.out.println("Source array name: " + meta.getTableName(0));
+		System.out.println(meta.getColumnCount() + " columns:");
+
+		IResultSetWrapper resWrapper = resultSet.unwrap(IResultSetWrapper.class);
+		for (int i = 1; i <= meta.getColumnCount(); i++) {
+			System.out.println(meta.getColumnName(i) + " - " + meta.getColumnTypeName(i) + " - is attribute:"
+					+ resWrapper.isColumnAttribute(i));
+		}
+
 		long numberOfCellsSciDB = 0;
 		while (resultSet.next()) {
-			System.out.println(resultSet.getString(3));
+			// System.out.println(resultSet.getString(3));
+			// System.out.println(resultSet.getInt(1));
 			++numberOfCellsSciDB;
 		}
+		resultSet.close();
+		query.close();
+		con.close();
 		assertEquals(numberOfRowsPostgres, numberOfCellsSciDB);
 	}
+	
+	
 
 }
