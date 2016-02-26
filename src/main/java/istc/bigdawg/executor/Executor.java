f1d75cccc43785840334de123be200eabc2038c8
Ankush Gupta <me@ankushg.com>
2016-02-26 14:28:04 -0500

    asynchronous QEP execution

--- src/main/java/istc/bigdawg/executor/Executor.java
+++ src/main/java/istc/bigdawg/executor/Executor.java
@@ -6,10 +6,21 @@ import istc.bigdawg.postgresql.PostgreSQLHandler.QueryResult;
 
 import java.sql.SQLException;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 
 public class Executor {
     public static QueryResult executePlan(QueryExecutionPlan plan) throws SQLException, MigrationException {
         return new PlanExecutor(plan).executePlan().orElse(null);
     }
+
+    public static CompletableFuture<Optional<QueryResult>> executePlanAsync(QueryExecutionPlan plan) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                return new PlanExecutor(plan).executePlan();
+            } catch (Exception e) {
+                throw new CompletionException(e);
+            }
+        });
     }
 }
