68ff3358505fc64bccd753416b25e4c34fa177b3
Adam Dziedzic <adam.dziedzi@gmail.com>
2016-02-29 12:35:36 -0600

    FromSciDBToPostgresImplementation.java  compiles

--- src/main/java/istc/bigdawg/migration/FromSciDBToPostgresImplementation.java
+++ src/main/java/istc/bigdawg/migration/FromSciDBToPostgresImplementation.java
@@ -371,6 +371,7 @@ public class FromSciDBToPostgresImplementation {
 					throw new MigrationException(e.getMessage());
 				}
 			}
+		}
 			/*
 			 * the file was created by SciDB and we cannot remove it (another
 			 * user)
