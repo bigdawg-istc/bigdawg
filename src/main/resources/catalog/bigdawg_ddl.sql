f33dcc06a2812d0ccf3874054af10b015795dce3
Zuohao She <Jack@Zuohaos-MacBook-Air.local>
2016-03-13 00:01:34 -0600

    Resolved content ambiguity associated with bigdawg_catalog and bigdawg_schema.

--- src/main/resources/catalog/bigdawg_ddl.sql
+++ src/main/resources/catalog/bigdawg_ddl.sql
@@ -1,3 +1,23 @@
+CREATE TABLE IF NOT EXISTS monitoring (
+      lastRan bigint,
+      duration integer,
+      island varchar(30),
+      query varchar(3000)
+);
+
+CREATE TABLE IF NOT EXISTS migrationstats (
+      migrationstatsID serial PRIMARY KEY,
+      fromLoc varchar(250),
+      toLoc varchar(250),
+      objectFrom varchar(250),
+      objectTo varchar(250),
+      startTime bigint,
+      endTime bigint,
+      countExtracted bigint,
+      countLoaded bigint,
+      message varchar(1000)
+);
+
 CREATE SCHEMA IF NOT EXISTS catalog;
 
 CREATE TABLE IF NOT EXISTS catalog.islands (
