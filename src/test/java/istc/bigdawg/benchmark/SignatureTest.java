4316a6d430c2435b4c4edd7242f7f52681931795
Ankush Gupta <me@ankushg.com>
2016-03-13 23:27:24 -0400

    refactored SignatureTest to calculate runtimes directly

--- src/test/java/istc/bigdawg/benchmark/SignatureTest.java
+++ src/test/java/istc/bigdawg/benchmark/SignatureTest.java
@@ -4,6 +4,7 @@ import istc.bigdawg.monitoring.Monitor;
 import istc.bigdawg.planner.Planner;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -15,40 +16,54 @@ import java.util.Map;
  * Created by ankush on 3/13/16.
  */
 public class SignatureTest {
+    private static Logger log = Logger.getLogger(SignatureTest.class);
 
-    public static Collection<String> trainingQueries = Arrays.asList(); // TODO(jack?)
-    public static Collection<String> testQueries = Arrays.asList(); // TODO(jack?)
-    
+    // TODO(jack) or someone with knowledge of the planner/signatures
+    private static final Collection<String> trainingQueries = Arrays.asList();
+
+    // TODO(jack) or someone with knowledge of the planner/signatures
+    private static final Collection<String> testQueries = Arrays.asList();
+
+    private final Map<String, Pair<Long, Long>> results = new HashMap<>();
+
     @Test
     public void testSignature() {
         runBenchmark();
-        processResults();
+        log.info(String.format("Completed benchmark with results (left is production, right is optimal): %s", results.toString()));
     }
 
     public void runBenchmark() {
-        setTrainingMode(true);
-        runQueries(trainingQueries);
+        // run the training queries in training mode to populate the monitor
+        runQueries(trainingQueries, true);
 
-        setTrainingMode(false);
-        runQueries(testQueries);
+        // run the test queries in production mode to get the signature-suggested plan
+        runQueries(testQueries, false);
 
-        setTrainingMode(true);
-        runQueries(testQueries);
+        // run the test queries in training mode to get the first-returned plan
+        runQueries(testQueries, true);
     }
 
-    public void setTrainingMode(boolean trainingMode) {
-        // TODO(jack): set system to proper mode of execution somehow
-        // either keep track of it in this test and allow it to be passed into the Planner
-        // or allow the Planner to keep track of it?
-    }
+    public void runQueries(Collection<String> tests, boolean isTrainingMode) {
+        // TODO(jack): some mechanism to control if the Planner is in training or expansive mode using isTrainingMode
 
-    public void runQueries(Collection<String> tests) {
         for(String test : testQueries) {
-            // this blocks until each one completes if in production mode
+            long runtime;
+            long start = System.currentTimeMillis();
             try {
                 Planner.processQuery(test);
+                runtime = System.currentTimeMillis() - start;
             } catch (Exception e) {
                 e.printStackTrace();
+                runtime = Long.MAX_VALUE;
+            }
+
+            Pair<Long, Long> result = results.getOrDefault(test, new ImmutablePair<>(Long.MAX_VALUE, Long.MAX_VALUE));
+            if(isTrainingMode) {
+                // runtime is fastest result of all possible plans
+                results.put(test, new ImmutablePair<>(result.getLeft(), runtime));
+            } else {
+                // runtime used signature model to predict plan
+                results.put(test, new ImmutablePair<>(runtime, result.getRight()));
             }
         }
 
@@ -60,21 +75,4 @@ public class SignatureTest {
             }
         }
     }
-
-    public Map<String, Pair<Long, Long>> processResults() {
-        Map<String, Pair<Long, Long>> results = new HashMap<>();
-
-        for(String query : testQueries) {
-            // TODO: get production time from monitor output file
-            long production = 0;
-
-            // TODO: get minimum time for this query from monitor output file
-            long bestFromTraining = 0;
-
-            results.put(query, ImmutablePair.of(production, bestFromTraining));
-        }
-
-        return results;
-    }
-
 }
