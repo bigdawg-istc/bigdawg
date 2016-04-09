9b7103d56e2dd750aa35345245dc87fa481f3cbe
Ankush Gupta <me@ankushg.com>
2016-04-09 13:48:05 -0400

    cleaned up Executor logging

--- src/main/java/istc/bigdawg/executor/PlanExecutor.java
+++ src/main/java/istc/bigdawg/executor/PlanExecutor.java
@@ -15,7 +15,7 @@ import com.google.common.collect.Multimaps;
 import istc.bigdawg.executor.plan.BinaryJoinExecutionNode;
 import istc.bigdawg.migration.MigrationResult;
 import org.apache.commons.lang3.tuple.ImmutablePair;
-import org.apache.log4j.Logger;
+import com.jcabi.log.Logger;
 
 import istc.bigdawg.exceptions.MigrationException;
 import istc.bigdawg.executor.plan.ExecutionNode;
@@ -25,8 +25,6 @@ import istc.bigdawg.monitoring.Monitor;
 import istc.bigdawg.postgresql.PostgreSQLHandler;
 import istc.bigdawg.postgresql.PostgreSQLHandler.QueryResult;
 import istc.bigdawg.query.ConnectionInfo;
-
-import java.util.Spliterators;
 
 /**
  * TODO:
@@ -38,7 +36,6 @@ import java.util.Spliterators;
  * @author ankush
  */
 class PlanExecutor {
-    static final Logger log = Logger.getLogger(PlanExecutor.class.getName());
     static final Monitor monitor = new Monitor();
 
     private final Multimap<ExecutionNode, ConnectionInfo> resultLocations = Multimaps.synchronizedSetMultimap(HashMultimap.create());
@@ -65,18 +62,19 @@ class PlanExecutor {
             sb.append(String.format("%s -> (%s)\n", n, plan.getDependents(n)));
         }
 
-        log.debug(String.format("Received plan %s: \n %s", plan.getSerializedName(), sb.toString()));
+        Logger.info(this, "Received plan %s", plan.getSerializedName());
 
-        log.debug(String.format("Ordered queries: \n %s",
-                StreamSupport.stream(Spliterators.spliterator(plan.iterator(), plan.vertexSet().size(), Spliterator.ORDERED), false)
+        Logger.debug(this, "Nodes for plan %s: \n %s", plan.getSerializedName(), sb);
+        Logger.debug(this, "Ordered queries: \n %s",
+                StreamSupport.stream(plan.spliterator(), false)
                     .map(ExecutionNode::getQueryString)
                     .filter(Optional::isPresent).map(opt -> opt.get())
-                    .collect(Collectors.joining(" \n ---- then ---- \n "))));
+                    .collect(Collectors.joining(" \n ---- then ---- \n ")));
 
         // initialize countdown latches to the proper counts
         for(ExecutionNode node : plan) {
             int latchSize = plan.inDegreeOf(node);
-            log.debug(String.format("Node %s lock initialized to %s", node.getTableName(), latchSize));
+            Logger.debug(this, "Node %s lock initialized with %s dependencies", node, latchSize);
             locks.put(node, new CountDownLatch(latchSize));
         }
     }
@@ -87,12 +85,12 @@ class PlanExecutor {
     public Optional<QueryResult> executePlan() throws SQLException, MigrationException {
         long start = System.currentTimeMillis();
 
-        log.debug(String.format("Executing query plan %s...", plan.getSerializedName()));
+        Logger.info(this, "Executing query plan %s...", plan.getSerializedName());
 
         CompletableFuture<Optional<QueryResult>> finalResult = CompletableFuture.completedFuture(Optional.empty());
 
         for (ExecutionNode node : plan) {
-            log.debug(String.format("Examining query node %s...", node.getTableName()));
+            Logger.debug(this, "Creating Future for query node %s...", node);
 
             CompletableFuture<Optional<QueryResult>> result = CompletableFuture.supplyAsync(() -> this.executeNode(node));
 
@@ -106,21 +104,21 @@ class PlanExecutor {
         try {
             result = finalResult.get();
         } catch (InterruptedException e) {
-            log.error(String.format("Execution of query plan %s was interrupted", plan.getSerializedName()), e);
+            Logger.error(this, "Execution of query plan %s was interrupted: %[exception]s", plan.getSerializedName(), e);
             Thread.currentThread().interrupt();
         } catch (ExecutionException e) {
-            log.error(String.format("Error retrieving results of final query node %s", plan.getSerializedName()), e);
+            Logger.error(this, "Error retrieving results of final query node %s: %[exception]s", plan.getSerializedName(), e);
         }
 
         dropTemporaryTables();
 
         long end = System.currentTimeMillis();
 
-        log.debug(String.format("Finished executing query plan %s, in %d seconds.", plan.getSerializedName(), (start - end) / 1000));
-        log.debug(String.format("Sending timing to monitor..."));
+        Logger.info(this, "Finished executing query plan %s, in %[ms]d.", plan.getSerializedName(), (start - end));
+        Logger.info(this, "Sending timing to monitor...");
         monitor.finishedBenchmark(plan, start, end);
 
-        log.debug(String.format("Returning result to planner..."));
+        Logger.info(this, "Returning result to planner...");
         return result;
     }
     
@@ -149,14 +147,14 @@ class PlanExecutor {
         // otherwise execute as local query execution (same as broadcast join)
         // colocate dependencies, blocking until completed
         colocateDependencies(node, Collections.emptySet());
-        log.debug(String.format("Executing query node %s...", node.getTableName()));
+        Logger.debug(this, "Executing query node %s...", node);
 
         return node.getQueryString().flatMap((query) -> {
             try {
                 Optional<QueryResult> result = ((PostgreSQLHandler) node.getEngine().getHandler()).executePostgreSQL(query);
                 return result;
             } catch (SQLException e) {
-                log.error(String.format("Error executing node %s", node.getTableName()), e);
+                Logger.error(this, "Error executing node %s: %[exception]s", node, e);
                 // TODO: if error is actually bad, don't markNodeAsCompleted, and instead fail the QEP gracefully.
                 return Optional.empty();
             } finally {
@@ -166,7 +164,7 @@ class PlanExecutor {
     }
 
     private void markNodeAsCompleted(ExecutionNode node) {
-        log.debug(String.format("Marking node %s as completed.", node.getTableName()));
+        Logger.debug(this, "Completed execution of %s.", node);
 
         if (!plan.getTerminalTableNode().equals(node)) {
             // clean up the intermediate table later
@@ -176,7 +174,7 @@ class PlanExecutor {
             resultLocations.put(node, node.getEngine());
 
             for (ExecutionNode dependent : plan.getDependents(node)) {
-                log.debug(String.format("Decrementing lock of dependency %s", dependent.getTableName()));
+                Logger.debug(this, "Decrementing lock of %s because $s completed.", dependent, node);
                 locks.get(dependent).countDown();
             }
         }
@@ -193,46 +191,55 @@ class PlanExecutor {
     private void colocateDependencies(ExecutionNode node, Collection<String> ignoreTables) {
         // Block until dependencies are all resolved
         try {
-            log.debug(String.format("Waiting for dependencies of query node %s to be resolved...", node.getTableName()));
+            Logger.debug(this, "Waiting for dependencies of query node %s to be resolved...", node);
             this.locks.get(node).await();
         } catch (InterruptedException e) {
-            log.error(String.format("Execution of query node %s was interrupted while waiting for dependencies.", node.getTableName()), e);
+            Logger.error(this, "Execution of query node %s was interrupted while waiting for dependencies: %[exception]s", node, e);
             Thread.currentThread().interrupt();
         }
 
-        log.debug(String.format("Colocating dependencies of query node %s", node.getEngine()));
+        Logger.debug(this, "Colocating dependencies of %s to %s", node, node.getEngine());
+
+        ignoreTables.addAll(plan.getDependencies(node).stream().filter(d -> resultLocations.containsEntry(d, node.getEngine())).map(n -> n.getTableName().orElse("NO_TABLE")).collect(Collectors.toSet()));
+
+        Logger.debug(this, "Ignoring dependencies %[list]s of %s", ignoreTables, node.getEngine());
 
         CompletableFuture[] futures = plan.getDependencies(node).stream()
-                .filter(d -> !resultLocations.containsEntry(d, node.getEngine()) && d.getTableName().isPresent() && !ignoreTables.contains(d.getTableName().get()))
+                .filter(d -> d.getTableName().isPresent() && !ignoreTables.contains(d.getTableName().get()))
                 .map((d) -> {
                     // computeIfAbsent gets a previous migration's Future, or creates one if it doesn't already exist
                     ImmutablePair<String, ConnectionInfo> migrationKey = new ImmutablePair<>(d.getTableName().get(), node.getEngine());
 
                     return migrations.computeIfAbsent(migrationKey, (k) -> {
-                        return CompletableFuture.supplyAsync(() -> colocateSingleDependency(d, node));
+                        return CompletableFuture.supplyAsync(() -> {
+                            MigrationResult result = colocateSingleDependency(d, node);
+                            Logger.debug(this, "Finished migrating dependency %s of node %s: %s", d, node, result);
+                            return result;
+                        });
                     });
                 }).toArray(CompletableFuture[]::new);
+
 
         CompletableFuture.allOf(futures).join();
     }
 
     private MigrationResult colocateSingleDependency(ExecutionNode dependency, ExecutionNode dependant) {
+        Logger.debug(this, "Migrating dependency %s from engine %s to engine %s...", dependency,
+                dependency.getEngine().getDatabase(), dependant.getEngine().getDatabase());
+
         return dependency.getTableName().map((table) -> {
-            log.debug(String.format("Migrating dependency table %s from engine %s to engine %s...", table, dependency.getEngine().getDatabase(),
-                    dependant.getEngine().getDatabase()));
             try {
                 MigrationResult result = Migrator.migrate(dependency.getEngine(), table, dependant.getEngine(), table);
 
                 // mark the dependency's data as being present on node.getEngine()
-                resultLocations.put(dependency  , dependant.getEngine());
+                resultLocations.put(dependency, dependant.getEngine());
 
                 // mark that this engine now has a copy of the dependency's data
                 temporaryTables.put(dependant.getEngine(), table);
 
-                log.debug(String.format("Finished migrating dependency %s of node %s!", dependency.getTableName(), dependant.getTableName()));
                 return result;
             } catch (MigrationException e) {
-                log.error(String.format("Error migrating dependency %s of node %s!", dependency.getTableName(), dependant.getTableName()), e);
+                Logger.error(this, "Error migrating dependency %s of node %s: %[exception]s", dependency.getTableName(), dependant.getTableName(), e);
                 return MigrationResult.getFailedInstance(e.getLocalizedMessage());
             }
         }).orElse(MigrationResult.getEmptyInstance(String.format("No table to migrate for node %s", dependency.getTableName())));
@@ -245,7 +252,7 @@ class PlanExecutor {
             for (ConnectionInfo c : temporaryTables.keySet()) {
                 Collection<String> tables = temporaryTables.get(c);
 
-                log.debug(String.format("Cleaning up %s by removing %s...", c, tables));
+                Logger.debug(this, "Cleaning up %s by removing %s...", c, tables);
                 ((PostgreSQLHandler) c.getHandler()).executeStatementPostgreSQL(c.getCleanupQuery(tables));
 
                 removed.putAll(c, tables);
@@ -256,7 +263,7 @@ class PlanExecutor {
             }
         }
 
-        log.debug(String.format("Temporary tables for query plan %s have been removed", plan.getSerializedName()));
+        Logger.debug(this, "Temporary tables for query plan %s have been cleaned up", plan.getSerializedName());
     }
 
 
@@ -271,14 +278,14 @@ class PlanExecutor {
     private void colocateDependenciesSerially(ExecutionNode node) {
         // Block until dependencies are all resolved
         try {
-            log.debug(String.format("Waiting for dependencies of query node %s to be resolved", node.getTableName()));
+            Logger.debug(this, String.format("Waiting for dependencies of query node %s to be resolved", node.getTableName()));
             this.locks.get(node).await();
         } catch (InterruptedException e) {
-            log.error(String.format("Execution of query node %s was interrupted while waiting for dependencies.", node.getTableName()), e);
+            Logger.error(this, "Execution of query node %s was interrupted while waiting for dependencies: %[exception]s", node.getTableName(), e);
             Thread.currentThread().interrupt();
         }
 
-        log.debug(String.format("Colocating dependencies of query node %s", node.getTableName()));
+        Logger.debug(this, String.format("Colocating dependencies of query node %s", node.getTableName()));
 
         plan.getDependencies(node).stream()
                 // only look at dependencies not already on desired engine
@@ -286,7 +293,7 @@ class PlanExecutor {
                 .forEach(d -> {
                     // migrate to node.getEngine()
                     d.getTableName().ifPresent((table) -> {
-                        log.debug(String.format("Migrating dependency table %s from engine %s to engine %s...", table, d.getEngine(),
+                        Logger.debug(this, String.format("Migrating dependency table %s from engine %s to engine %s...", table, d.getEngine(),
                                 node.getEngine()));
                         try {
                             Migrator.migrate(d.getEngine(), table, node.getEngine(), table);
@@ -296,7 +303,7 @@ class PlanExecutor {
                             // mark that this engine now has a copy of the dependency's data
                             temporaryTables.put(node.getEngine(), table);
                         } catch (MigrationException e) {
-                            log.error(String.format("Error migrating dependency %s of node %s!", d.getTableName(), node.getTableName()), e);
+                            Logger.error(this, "Error migrating dependency %s of node %s: %[exception]s", d, node, e);
                         }
                     });
                 });
