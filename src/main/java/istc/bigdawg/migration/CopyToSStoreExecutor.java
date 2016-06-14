cf257f05434c54d60d2b28243d2b4b757fc52be6
john <john@cs.brown.edu>
2016-06-14 11:21:34 -0400

    Postgres -> S-Store

---
+++ src/main/java/istc/bigdawg/migration/CopyToSStoreExecutor.java
@@ -0,0 +1,99 @@
+/**
+ * 
+ */
+package istc.bigdawg.migration;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.concurrent.Callable;
+
+import org.apache.log4j.Logger;
+import org.postgresql.copy.CopyManager;
+import org.postgresql.core.BaseConnection;
+
+import istc.bigdawg.sstore.SStoreSQLConnectionInfo;
+import istc.bigdawg.sstore.SStoreSQLHandler;
+import istc.bigdawg.utils.LogUtils;
+import istc.bigdawg.utils.StackTrace;
+
+public class CopyToSStoreExecutor implements Callable<Long> {
+
+	private static Logger log = Logger.getLogger(CopyToSStoreExecutor.class);
+
+//	private CopyManager cpTo;
+	private String copyToString;
+	private InputStream input;
+	private String inputFile;
+	private Connection connection;
+	final String tableName;
+	final String trim;
+
+	public CopyToSStoreExecutor(SStoreSQLConnectionInfo connectionSStoreSQL,
+			final String copyToString, final String inputFile, 
+			final String tableName, final String trim) throws SQLException {
+		this.connection = SStoreSQLHandler.getConnection(connectionSStoreSQL);
+		this.copyToString = copyToString;
+		this.inputFile = inputFile;
+		this.tableName = tableName;
+		this.trim = trim;
+		this.input = null;
+//		this.cpTo = new CopyManager((BaseConnection) connection);
+	}
+
+	public CopyToSStoreExecutor(SStoreSQLConnectionInfo connectionSStoreSQL,
+			final String copyToString, InputStream input,
+			final String tableName, final String trim) throws SQLException {
+		this.connection = SStoreSQLHandler.getConnection(connectionSStoreSQL);
+		this.copyToString = copyToString;
+		this.input = input;
+		this.tableName = tableName;
+		this.trim = trim;
+//		this.cpTo = new CopyManager((BaseConnection) connection);
+	}
+
+	/**
+	 * Copy data to S-Store.
+	 * 
+	 * @return number of loaded rows
+	 */
+	public Long call() {
+		log.info("start call: Copy to S-Store (Executor)");
+    	System.out.println("copy from expert has been called! inputfile is: " + inputFile);
+		if (input == null) {
+			try {
+				input = new BufferedInputStream(new FileInputStream(inputFile));
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+				String msg = e.getMessage()
+						+ " Problem with thread for PostgreSQL copy manager "
+						+ "while loading data from PostgreSQL.";
+				log.error(msg + StackTrace.getFullStackTrace(e), e);
+				return -1L;
+			}
+		}
+		Long countLoadedRows = 0L;
+		try {
+			SStoreSQLHandler.executePreparedImportStatement(connection, copyToString, tableName, input, trim);
+//			connection.commit();
+//			countLoadedRows = cpTo.copyIn(copyToString, input);
+			input.close();
+			connection.commit();
+		} catch (IOException | SQLException e) {
+			String msg = e.getMessage()
+					+ " Problem with thread for S-Store copy manager "
+					+ "while copying data to S-Store.";
+			log.error(LogUtils.replace(msg));
+			e.printStackTrace();
+		}
+		return countLoadedRows;
+	}
+	
+}
---
+++ src/main/java/istc/bigdawg/migration/FromPostgresToSStore.java
@@ -0,0 +1,138 @@
+package istc.bigdawg.migration;
+
+import static istc.bigdawg.network.NetworkUtils.isThisMyIpAddress;
+
+import java.net.InetAddress;
+import java.sql.*;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+
+import istc.bigdawg.LoggerSetup;
+import istc.bigdawg.exceptions.MigrationException;
+import istc.bigdawg.network.NetworkOut;
+import istc.bigdawg.postgresql.PostgreSQLConnectionInfo;
+import istc.bigdawg.query.ConnectionInfo;
+import istc.bigdawg.sstore.SStoreSQLConnectionInfo;
+
+public class FromPostgresToSStore 
+	implements FromDatabaseToDatabase, MigrationNetworkRequest {
+
+    private static final long serialVersionUID = 1L;
+    
+    
+    private static Logger log = Logger.getLogger(FromPostgresToSStore.class);
+
+    private PostgreSQLConnectionInfo connectionFrom;
+    private String fromTable;
+    private SStoreSQLConnectionInfo connectionTo;
+    private String toTable;
+
+
+    @Override
+    public MigrationResult execute() throws MigrationException {
+	if (this.connectionFrom == null || this.fromTable == null
+		|| this.connectionTo == null || this.toTable == null) {
+	throw new MigrationException("The object was not initialized");
+        }
+        FromPostgresToSStoreImplementation migrator = new FromPostgresToSStoreImplementation(
+        		connectionFrom, fromTable, connectionTo, toTable);
+        try {
+			return migrator.migrateBin();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+    }
+
+    @Override
+    public MigrationResult migrate(ConnectionInfo connectionFrom, String objectFrom, ConnectionInfo connectionTo,
+	    String objectTo) throws MigrationException {
+	log.debug("General data migration: " + this.getClass().getName());
+	if (connectionFrom instanceof PostgreSQLConnectionInfo
+			&& connectionTo instanceof SStoreSQLConnectionInfo) {
+		this.connectionFrom = (PostgreSQLConnectionInfo) connectionFrom;
+		this.fromTable = objectFrom;
+		this.connectionTo = (SStoreSQLConnectionInfo) connectionTo;
+		this.toTable = objectTo;
+		try {
+			/*
+			 * check if the address is not a local host
+			 */
+			String hostname = connectionFrom.getHost();
+			log.debug("Postgres hostname: " + hostname);
+			if (!isThisMyIpAddress(InetAddress.getByName(hostname))) {
+				log.debug("Migration will be executed remotely.");
+				Object result = NetworkOut.send(this, hostname);
+				return processResult(result);
+			}
+			/* execute the migration locally */
+			log.debug("Migration will be executed locally.");
+			return execute();
+		} catch (Exception e) {
+			throw new MigrationException(e.getMessage(), e);
+		}
+	}
+	return null;
+    }
+    
+    /**
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+//		LoggerSetup.setLogging();
+		FromPostgresToSStore migrator = new FromPostgresToSStore();
+		PostgreSQLConnectionInfo conFrom = new PostgreSQLConnectionInfo(
+				"localhost", "5430", "test_db", "pguser", "");
+		String tableFrom = "CUSTOMER";
+		SStoreSQLConnectionInfo conTo = new SStoreSQLConnectionInfo("localhost",
+				"21212", "", "user", "password");
+		String tableTo = "CUSTOMER";
+		long startTime = System.currentTimeMillis();
+		MigrationResult result = migrator.migrate(conFrom, tableFrom, conTo, tableTo);
+		long endTime = System.currentTimeMillis();
+		System.out.println("time duration is: " + (endTime - startTime));
+		System.out.println(result);
+		
+//		Properties props = new Properties();
+//		props.setProperty("user","pguser");
+//		props.setProperty("password","test");
+//		Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5430/tpch", props);
+//		startTime = System.currentTimeMillis();
+//		String sql1 = "select count(*) from customer";
+//		Statement stmt1 = conn.createStatement();
+//		ResultSet results1 = stmt1.executeQuery(sql1);
+//		while (results1.next()) {
+//			System.out.println(results1.getInt(1));
+//		}
+//		stmt1.close();
+//		results1.close();
+//		conn.close();
+//		endTime = System.currentTimeMillis();
+//		System.out.println("time duration is: " + (endTime - startTime));
+
+//		Class.forName("org.voltdb.jdbc.Driver");
+//		conn = DriverManager.getConnection("jdbc:voltdb://localhost:21212");
+//		String sql = "Select count(*) from orders;";
+//		Statement stmt = conn.createStatement();
+//		ResultSet results = stmt.executeQuery(sql);
+//		while(results.next()) {	
+//			System.out.println(results.getInt(1));
+//		}
+		
+		
+		
+//		Properties props = new Properties();
+//		props.setProperty("user","pguser");
+//		props.setProperty("password","test");
+//		Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/test_db", props);
+//		long startTime = System.currentTimeMillis();
+//        String sql1 = "select * from orders union select * from orders1";
+//        Statement stmt1 = conn.createStatement();
+//        ResultSet results1 = stmt1.executeQuery(sql1);
+//        long endTime = System.currentTimeMillis();
+//        System.out.println("time duration is: " + (endTime - startTime));
+	}
+
+}
