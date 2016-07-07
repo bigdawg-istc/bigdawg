ec418eb1026ced8d783de8e919060b5eba4b2171
Albert Carter <albert.r.carter@gmail.com>
2016-07-07 18:35:02 -0400

    expose postgres ports on vagrant

--- provisions/docker/create-dev-containers.sh
+++ provisions/docker/create-dev-containers.sh
@@ -7,10 +7,12 @@ echo "Creating Docker containers..."
 echo "(n/m) Creating \"postgres1\" - Postgres server. Exposed at postgres1:5432"
 docker create --name postgres1 \
     --net=bigdawg \
+    -p 5431:5432 \
     postgres1
 echo "(n/m) Creating \"postgres2\" - Postgres server. Exposed at postgres2:5432"
 docker create --name postgres2 \
     --net=bigdawg \
+    -p 5430:5432 \
     postgres2
 echo "(n/m) Creating \"maven\" - Java Maven container. Exposed at 0.0.0.0:8188"
 docker create --name maven \
