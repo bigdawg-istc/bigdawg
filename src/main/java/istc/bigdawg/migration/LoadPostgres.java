6c611ef75ea4965701896e0273fbd120ce276dad
Adam Dziedzic <adam.dziedzi@gmail.com>
2016-07-22 17:37:39 +0200

    quick fix for LoadPostgres - null for fromHandler

--- src/main/java/istc/bigdawg/migration/LoadPostgres.java
+++ src/main/java/istc/bigdawg/migration/LoadPostgres.java
@@ -208,19 +208,21 @@ public class LoadPostgres implements Load {
 			if (!new PostgreSQLHandler(migrationInfo.getConnectionTo(),
 					connection).existsTable(migrationInfo.getObjectTo())) {
 				ObjectMetaData objectFromMetaData = null;
-				try {
-					objectFromMetaData = fromHandler
-							.getObjectMetaData(migrationInfo.getObjectFrom());
-				} catch (Exception e) {
-					throw new MigrationException(e.getMessage(), e);
+				if (fromHandler != null) {
+					try {
+						objectFromMetaData = fromHandler.getObjectMetaData(
+								migrationInfo.getObjectFrom());
+					} catch (Exception e) {
+						throw new MigrationException(e.getMessage(), e);
+					}
+					List<AttributeMetaData> attributes = objectFromMetaData
+							.getAllAttributesOrdered();
+					String createTableStatement = PostgreSQLHandler
+							.getCreatePostgreSQLTableStatement(
+									migrationInfo.getObjectTo(), attributes);
+					PostgreSQLHandler.createTargetTableSchema(connection,
+							migrationInfo.getObjectTo(), createTableStatement);
 				}
-				List<AttributeMetaData> attributes = objectFromMetaData
-						.getAllAttributesOrdered();
-				String createTableStatement = PostgreSQLHandler
-						.getCreatePostgreSQLTableStatement(
-								migrationInfo.getObjectTo(), attributes);
-				PostgreSQLHandler.createTargetTableSchema(connection,
-						migrationInfo.getObjectTo(), createTableStatement);
 			}
 		} catch (SQLException e) {
 			new MigrationException(e.getMessage(), e);
