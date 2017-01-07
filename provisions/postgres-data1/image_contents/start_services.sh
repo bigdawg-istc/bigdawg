0ba0c9a92304146e90a86f6ca322d174ef622c07
Kyle O'Brien <ky19046@mit.edu>
2017-01-07 01:58:06 +0000

    start_services.sh edited online with Bitbucket

--- provisions/postgres-data1/image_contents/start_services.sh
+++ provisions/postgres-data1/image_contents/start_services.sh
@@ -6,6 +6,7 @@ echo "STARTING POSTGRES SERVER IN BACKGROUND"
 /usr/lib/postgresql/9.4/bin/postgres -D /var/lib/postgresql/9.4/main -p ${PGPORT} -c config_file=/etc/postgresql/9.4/main/postgresql.conf 2>&1 &
 
 # echo "STARTING BIGDAWG"
+# cd /image_contents
 # java -classpath "istc.bigdawg-1.0-SNAPSHOT-jar-with-dependencies.jar" istc.bigdawg.Main ${BDHOST}
 
 tail -f /dev/null
