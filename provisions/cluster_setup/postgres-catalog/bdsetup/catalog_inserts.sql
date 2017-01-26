e692c7eacade5eea9eb96470017e5b92970da99b
Kyle OBrien <kyleobrien@saw.csail.mit.edu>
2017-01-25 19:04:05 -0500

    Fixed D4M insertion code

--- provisions/cluster_setup/postgres-catalog/bdsetup/catalog_inserts.sql
+++ provisions/cluster_setup/postgres-catalog/bdsetup/catalog_inserts.sql
@@ -23,6 +23,14 @@ insert into catalog.shims values (1, 0, 1, 'N/A');
 insert into catalog.shims values (2, 0, 2, 'N/A');
 insert into catalog.shims values (3, 1, 3, 'N/A');
 
+-- new values for accumulo
+insert into catalog.islands values (2, 'TEXT', 'JSON');
+insert into catalog.engines values (4, 'saw ZooKeeper', '128.30.76.163', 2181, 'Accumulo 1.6');
+insert into catalog.shims values (4, 2, 4, 'N/A');
+-- ALTER TABLE catalog.databases ALTER COLUMN password TYPE varchar(30);
+insert into catalog.databases values (7, 4, 'accumulo', 'bigdawg', 'bigdawg');
+insert into catalog.objects values (47, 'pythontest', '', 7, 7);
+
 -- catalog.scidbbinapath:
 -- binary path to scidb utilities: csv2scidb, iquery, etc.
 insert into catalog.scidbbinpaths values (3,'/opt/scidb/14.12/bin/');
